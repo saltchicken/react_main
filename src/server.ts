795c6198208d5769e28afc8f041e6a0063ffdc67
saltchicken <john.eicher89@gmail.com>
2024-10-18 10:33:06 -0700

    Base for SQL interaction

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+// Deno.serve((_req) => {
+//   return new Response("Hello, world");
+// });
+//
+
+async function handleRequest(request: Request): Promise<Response> {
+  const { method, url } = request;
+  console.log(method);
+  console.log(url);
+
+  if (method === "GET" && url === "http://10.0.0.3:8000/data") {
+    // Simulate some data, for example, querying from a database
+    const data = { message: "Hello, world!" };
+
+    return new Response(JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } else {
+    // Handle 404 Not Found
+    return new Response("Not Found", { status: 404 });
+  }
+}
+
+Deno.serve(
+  {
+    port: 5443,
+    cert: Deno.readTextFileSync("/home/saltchicken/cert.pem"),
+    key: Deno.readTextFileSync("/home/saltchicken/key.pem"),
+  },
+  handleRequest,
+);
