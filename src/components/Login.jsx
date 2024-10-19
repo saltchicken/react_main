5bab5435e506650d936e8bc08b461bd973aa14b1
saltchicken <john.eicher89@gmail.com>
2024-10-19 13:26:42 -0700

    Fix login page logic. Replace 'window' with 'globalThis'

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,34 +4,33 @@ import Cookies from "js-cookie";
 
 export default function Login() {
   const navigate = useNavigate();
-  const [isLoggedin, setIsLoggedin] = useState(false);
 
   const handleClick = () => {
     // const callbackUrl = `${window.location.origin}`;
-    const callbackUrl = `${window.location.origin}/login`;
+    const callbackUrl = `${globalThis.location.origin}/login`;
     const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
     const targetUrl = `https://accounts.google.com/o/oauth2/auth?redirect_uri=${encodeURIComponent(
       callbackUrl,
     )}&response_type=token&client_id=${googleClientId}&scope=openid%20email%20profile`;
-    window.location.href = targetUrl;
+    globalThis.location.href = targetUrl;
   };
 
   useEffect(() => {
+    if (Cookies.get("access_token")) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  useEffect(() => {
     const accessTokenRegex = /access_token=([^&]+)/;
-    const isMatch = window.location.href.match(accessTokenRegex);
+    const isMatch = globalThis.location.href.match(accessTokenRegex);
 
     if (isMatch) {
       const accessToken = isMatch[1];
       Cookies.set("access_token", accessToken);
-      setIsLoggedin(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isLoggedin) {
       navigate("/");
     }
-  }, [isLoggedin, navigate]);
+  }, [navigate]);
 
   return (
     <div className="root">
