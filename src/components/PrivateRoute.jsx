c0d60526e6b0af7f9851dd244272d342047a2a6b
saltchicken <john.eicher89@gmail.com>
2024-10-20 16:31:12 -0700

    Fix PrivateRoute checking for valid user

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,14 +1,46 @@
-import { Navigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+// import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
 const PrivateRoute = ({ children }) => {
-  const accessToken = Cookies.get("access_token");
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(null); // null = loading, true = authenticated, false = not authenticated
 
-  if (!accessToken) {
-    return <Navigate to="/login" />;
+  useEffect(() => {
+    (async () => {
+      const accessToken = Cookies.get("access_token");
+      if (!accessToken) {
+        navigate("/login");
+        // return <Navigate to="/login" />;
+      }
+      const response = await fetch(`/api/auth/`);
+      const responseJSON = await response.json();
+
+      if (responseJSON) {
+        setIsAuthenticated(true);
+        console.log("Verified");
+      } else {
+        setIsAuthenticated(false);
+        console.log("Not Verified");
+      }
+    })();
+  }, [navigate]);
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
   }
 
-  return children;
+  if (!isAuthenticated) {
+    // navigate("/login");
+    console.log("hello there");
+    return <div>Not authenticated...</div>;
+    // return <Navigate to="/login" />;
+  }
+
+  if (isAuthenticated) {
+    return children;
+  }
 };
 
 export default PrivateRoute;
